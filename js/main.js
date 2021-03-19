4e69f51367acda5079731ccb5f80669e47f5a4bd
nayara1alka <nayaraalka@gmail.com>
2021-03-19 09:24:19 -0300

    versão 0.1

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function assistir(){
+    window.open('https://www.youtube.com/watch?v=PnqUs3xiAVI');
+}
+function maisInfo(){
+    window.location.href = "pagina-info.html";
+}
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
